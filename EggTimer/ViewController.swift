09046fc2d361fa2be809f09ee1b15162c24cbeda
Justin Knoetzke <justin@shampoo.ca>
2020-04-13 14:57:46 -0400

    Initial Commit

---
+++ EggTimer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by Justin Knoetzke on 2020-04-13.
+//  Copyright © 2020 Justin Knoetzke. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
